5cb3a3dc3a995c9cf964dfcd9a0fd0f33ae63921
PondJaᵀᴴ <palapon2545@gmail.com>
2022-11-09 01:18:52 +0700

    [B] Added Inventory endpoint
    + Added GET /inventory/
    + Added GET /inventory/id/<id>
    + Added GET /inventory/type/<type>
    + Added POST /inventory/update/
    + Added POST /inventory/add/

--- backend/app.js
+++ backend/app.js
@@ -146,6 +146,128 @@ app.post('/auth/login', function(req, res) {
     run().catch(console.dir);
 });
 
+// GET /inventory : return all inventory items
+app.get('/inventory', function(req, res) {
+    console.log(`GET /inventory`);
+    res.set("Content-Type", "application/json");
+    async function run() {
+        const client = new mongodb.MongoClient(mongoServerURI);
+        try {
+            const database = client.db('WAD');
+            const inventory = database.collection('inventory');
+            const result = await inventory.find({}).toArray();
+            res.end(JSON.stringify(result, null, 4));
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+// GET /inventory/id/<id> : return a inventory item
+app.get('/inventory/id/:id', function(req, res) {
+    console.log(`GET /inventory/${req.params.id}`);
+    res.set("Content-Type", "application/json");
+    async function run() {
+        const client = new mongodb.MongoClient(mongoServerURI);
+        try {
+            const database = client.db('WAD');
+            const inventory = database.collection('inventory');
+            const result = await inventory.find({_id: mongodb.ObjectId(req.params.id)}).toArray();
+            res.end(JSON.stringify(result, null, 4));
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+// GET /inventory/type/<type> : return all items in specified inventory type
+app.get('/inventory/type/:type', function(req, res) {
+    console.log(`GET /inventory/${req.params.type}`);
+    res.set("Content-Type", "application/json");
+    async function run() {
+        const client = new mongodb.MongoClient(mongoServerURI);
+        try {
+            const database = client.db('WAD');
+            const inventory = database.collection('inventory');
+            const result = await inventory.find({type: req.params.type}).toArray();
+            res.end(JSON.stringify(result, null, 4));
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+// POST /inventory/update : need id, name, email, password => return the updated data -or- empty if not valid ID 
+app.post('/inventory/update', function(req, res) {
+    console.log(`POST /inventory/update`);
+    res.set("Content-Type", "application/json");
+    async function run() {
+        const client = new mongodb.MongoClient(mongoServerURI);
+        try {
+            const database = client.db('WAD');
+            const inventory = database.collection('inventory');
+            let data = {
+                $set: {
+                    _id: req.body.id,
+                    name: req.body.name,
+                    type: req.body.type,
+                    lot: req.body.lot
+                }
+            };
+            if (req.body["_id"] == undefined || req.body["_id"] == null || req.body["_id"] == "") {
+                //ID can't be empty
+                res.end(JSON.stringify({}, null, 4));
+            } else {
+                const result = await inventory.updateOne({_id: mongodb.ObjectId(req.body.id)}, data,{upsert: 0});
+                if (result.modifiedCount > 0) {
+                    res.end(JSON.stringify({id: req.body.id, name: req.body.name, type: req.body.type, lot: req.body.lot}, null, 4));
+                } else {
+                    res.end(JSON.stringify({}, null, 4));
+                }
+            }
+        } catch (e) {
+            res.end(JSON.stringify({}, null, 4));
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+// POST /inventory/add : need name, email, password => return the inventory data
+app.post('/inventory/add', function(req, res) {
+    console.log(`POST /inventory/add`);
+    res.set("Content-Type", "application/json");
+    async function run() {
+        const client = new mongodb.MongoClient(mongoServerURI);
+        try {
+            const database = client.db('WAD');
+            const inventory = database.collection('inventory');
+            let data = {
+                $set: {
+                    name: req.body.name,
+                    type: req.body.type,
+                    lot: req.body.lot
+                }
+            };
+            const result = await inventory.insertOne(data);
+            if (result.acknowledged) {
+                res.end(JSON.stringify({id: result.insertedId, name: req.body.name, type: req.body.type, lot: req.body.lot}, null, 4));
+            } else {
+                res.end(JSON.stringify({}, null, 4));
+            }
+        } catch (e) {
+            res.end(JSON.stringify({}, null, 4));
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
 let server = app.listen(27777, function() {
     let port = server.address().port;
     console.log("listening on port " + port);
