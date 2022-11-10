492d5e7af77b46665b18877517491042fb581d30
PondJaᵀᴴ <palapon2545@gmail.com>
2022-11-10 16:43:14 +0700

    [B] Strict charset

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.all('*',function(req,res,next)
     res.set('Access-Control-Allow-Origin','*');
     res.set('Access-Control-Allow-Methods','GET,POST,DELETE');
     res.set('Access-Control-Allow-Headers','X-Requested-With,Content-Type');
+    res.set('Content-Type', 'application/json; charset=utf-8');
 
     if ('OPTIONS' == req.method) return res.send(200);
 
